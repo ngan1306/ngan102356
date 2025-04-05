137390360630e7619c1ac2c0149a864857fa7488
Your Name <your_email@example.com>
2025-04-05 08:37:27 +0700

    ffffiry

--- controllers/product_controler.js
+++ controllers/product_controler.js
@@ -1,27 +1,44 @@
-const categories = [
-  {
-    id: 1,
-    name: "Điện thoại",
-    description: "Các dòng điện thoại thông minh và phụ kiện",
-  },
-  {
-    id: 2,
-    name: "Laptop",
-    description: "Laptop các hãng khác nhau",
-  },
-  {
-    id: 3,
-    name: "Phụ kiện",
-    description: "Tai nghe, sạc, cáp,...",
-  },
-];
-
 const Product = require("../model/product");
 
 class ProductController {
+  static async create(req, res) {
+    let { name, description, price, stock } = req.body;
+    try {
+      let product = await Product.create({ name, description, price, stock });
+      res.redirect("/products");
+    } catch (error) {
+      res.render("product_new");
+    }
+  }
+  static async new(req, res) {
+    console.log("new");
+    res.render("product_new");
+  }
   static async index(req, res) {
-    const products = await Product.find();
+    let q = req.query.q;
+    let page = parseInt(req.query.page);
+    let skip = (page - 1) * 5;
+    let products;
+    if (q) {
+      products = await Product.find({ name: { $regex: q, $options: "i" } });
+    } else {
+      products = await Product.find().skip(skip).limit(5);
+    }
+
     res.render("product", { products });
+  }
+
+  static async delete(req, res) {
+    console.log(req.params.id);
+    let id = req.params.id;
+    let product = await Product.deleteOne({ _id: id });
+    res.redirect("/products");
+  }
+  static async edit(req, res) {
+    console.log(req.params.id);
+    let id = req.params.id;
+    let product = await Product.deleteOne({ _id: id });
+    res.redirect("/products");
   }
 }
 
--- routes/product.js
+++ routes/product.js
@@ -4,5 +4,9 @@ const ProductController = require("../controllers/product_controler");
 const router = express.Router();
 
 router.get("/", ProductController.index);
+router.get("/new", ProductController.new);
+router.post("/create", ProductController.create);
+
+router.get("/delete/:id", ProductController.delete);
 
 module.exports = router;
